ca66f2455d8be2cc3fc1f3a8b7962550ac82340f
Rupert Hunz <ruperthunz@Air-von-Rupert.fritz.box>
2025-02-14 00:18:24 +0100

    added fret dropdowns

--- src/components/settings/FretsSetting.jsx
+++ src/components/settings/FretsSetting.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { Context } from "../../App"
 
 // const octaves = ["firstOctave", "secondOctave", "thirdOctave"]
+const dropdowns = [false, false, false]
 
 export function FretsSetting() {
   const { t, language, frets, setFrets } = useContext(Context)
@@ -11,25 +12,38 @@ export function FretsSetting() {
       <div className="widget-label">{t.frets}</div>
       <div className="widget-content">
         <div className="btn-container">
-          {frets.map(octave => {
+          {frets.map((octave, index) => {
             return (
-              <div
-                key={octave.English}
-                // className={octave.allSelected ? "btn selected" : "btn"}
-                className="btn"
-                // onClick={() => setFrets()}
-              >
+              <div key={octave.English} className="btn fret-btn">
                 <div className={octave.allSelected ? "btn selected" : "btn"}>
                   {octave[language]}
                 </div>
-                <div className="btn plus-icon-container">
-                  <div className="plus-icon-line plus-icon-line1"></div>
-                  <div className="plus-icon-line plus-icon-line2"></div>
+                <div
+                  className="btn plus-icon-container"
+                  onClick={() => toggleDropdown(octave, index)}
+                >
+                  <div
+                    className={octave.open ? "plus-icon rotate45" : "plus-icon"}
+                  >
+                    <div className="plus-icon-line plus-icon-line1"></div>
+                    <div className="plus-icon-line plus-icon-line2"></div>
+                  </div>
                 </div>
-                <div className="fret-dropdown-container">
+                <div
+                  className={
+                    octave.open
+                      ? "fret-dropdown-container show"
+                      : "fret-dropdown-container"
+                  }
+                >
                   {octave.frets.map((fret, index) => {
                     return (
-                      <div key={crypto.randomUUID()} className="fret-choice">
+                      <div
+                        key={crypto.randomUUID()}
+                        className={
+                          fret ? "fret-choice selected" : "fret-choice"
+                        }
+                      >
                         {index + 1}
                       </div>
                     )
@@ -42,4 +56,12 @@ export function FretsSetting() {
       </div>
     </div>
   )
+
+  function toggleDropdown(octave, index) {
+    const octaveToChange = frets.find(oct => oct.id === index)
+    octaveToChange.open = !octaveToChange.open
+    setFrets(currentFrets => {
+      return currentFrets.toSpliced(index, 1, octaveToChange)
+    })
+  }
 }
--- src/frets.js
+++ src/frets.js
@@ -1,67 +1,9 @@
-// export const fretStates = {
-//   firstOctave: {
-//     english: "1st oct.",
-//     german: "1. Okt.",
-//     allSelected: true,
-//     frets: [
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true
-//     ]
-//   },
-//   secondOctave: {
-//     english: "2nd oct.",
-//     german: "2. Okt.",
-//     allSelected: true,
-//     frets: [
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true,
-//       true
-//     ]
-//   },
-//   thirdOctave: {
-//     english: "3rd oct.",
-//     german: "3. Okt.",
-//     allSelected: false,
-//     frets: [
-//       false,
-//       false,
-//       false,
-//       false,
-//       false,
-//       false,
-//       false,
-//       false,
-//       false,
-//       false,
-//       false,
-//       false
-//     ]
-//   }
-// }
-
 export const fretStates = [
   {
     English: "1st oct.",
     German: "1. Okt.",
+    id: 0,
+    open: false,
     allSelected: true,
     frets: [
       true,
@@ -81,6 +23,8 @@ export const fretStates = [
   {
     English: "2nd oct.",
     German: "2. Okt.",
+    id: 1,
+    open: false,
     allSelected: true,
     frets: [
       true,
@@ -100,6 +44,8 @@ export const fretStates = [
   {
     English: "3rd oct.",
     German: "3. Okt.",
+    id: 2,
+    open: false,
     allSelected: false,
     frets: [
       false,
