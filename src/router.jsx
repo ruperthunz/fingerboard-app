b6ad2acb84dceddcd18082633f7cdeb08352532b
Rupert Hunz <ruperthunz@Air-von-Rupert.fritz.box>
2025-05-09 21:14:20 +0200

    changed browser router to hash router

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createHashRouter } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Violin } from "./pages/Violin"
 import { Viola } from "./pages/Viola"
 import { Cello } from "./pages/Cello"
 import { Bass } from "./pages/Bass"
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   { path: "/", element: <Home /> },
   { path: "/violin", element: <Violin instrument="Violin" /> },
   { path: "/viola", element: <Viola instrument="Viola" /> },
